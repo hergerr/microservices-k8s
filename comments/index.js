aa6e3bfc39405cb3ea4e096f150265864d4b3d90
Tymoteusz Frankiewicz <241255@student.pwr.edu.pl>
2022-01-12 14:24:29 +0100

    Base query and event bus setup

--- comments/index.js
+++ comments/index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const bodyParser = require('body-parser')
-const { randomBytes } = require('crypto');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(cors());
@@ -9,21 +10,35 @@ app.use(bodyParser.json());
 
 const commentsByPostId = {};
 
-app.get('/posts/:id/comments', (req, res) => {
+app.get("/posts/:id/comments", (req, res) => {
   res.send(commentsByPostId[req.params.id] || []);
-})
+});
 
-app.post('/posts/:id/comments', (req, res) => {
-  const commentId = randomBytes(4).toString('hex');
+app.post("/posts/:id/comments", async (req, res) => {
+  const commentId = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content })
+  comments.push({ id: commentId, content });
+  await axios.post("htto://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: commentId,
+      content,
+      postId: req.params.id,
+    },
+  });
   commentsByPostId[req.params.id] = comments;
   res.status(201).send(comments);
-})
+});
+
+app.post("/events", (req, res) => {
+  console.log("received event: ", req.body.type);
+
+  res.send({});
+});
 
 app.listen(4001, () => {
-  console.log('Listening at 4001')
-})
+  console.log("Listening at 4001");
+});
